b578d9f7138779d4026e5f6ba4157629dd2b5439
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-02-23 22:50:28 +0900

    Arrays vs sets vs tuples

--- DAY2/DAY2/ViewController.swift
+++ DAY2/DAY2/ViewController.swift
@@ -33,6 +33,15 @@ class ViewController: UIViewController {
         var name = (first: "Taylor", last: "Swift")
         print(name.0)
         print(name.last)
+        
+        // Arrays vs sets vs tuples
+        // 各項目が正確な位置または名前を持つ、関連する値の特定の固定コレクションが必要な場合は、タプルを使用
+        let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+        // 一意である必要がある値のコレクションが必要な場合、または特定の項目がそこにあるかどうかを迅速に確認できるようにする必要がある場合は、セットを使用
+        let set = Set(["aardvark", "astronaut", "azalea"])
+        // 重複を含む可能性のある値のコレクションが必要な場合、または項目の順序が重要な場合は、配列を使用
+        let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+        print("address:\(address), set:\(set), pythons:\(pythons)")
     }
 
 
