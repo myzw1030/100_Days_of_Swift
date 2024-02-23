0e87fc60c4177d5f5cc155ca8a70b999eb9c90ef
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-02-23 18:32:15 +0900

    Array,Set

---
+++ DAY2/DAY2/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  DAY2
+//
+//  Created by USER on 2024/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Array
+        let john = "John Lennon"
+        let paul = "Paul McCarthey"
+        let george = "George Harrison"
+        let ringo = "Ringo Starr"
+        
+        let beatles = [john, paul, george, ringo]
+        print(beatles)
+        print(beatles[1])
+        
+        // Sets
+        let colors = Set(["red", "green", "blue"])
+        let colors1 = ["red", "green", "blue"]
+        print(colors)
+        print(colors1)
+        let colors2 = Set(["red", "green", "blue", "red", "blue"])
+        print(colors2)
+        
+        // Tuples
+    }
+
+
+}
+
