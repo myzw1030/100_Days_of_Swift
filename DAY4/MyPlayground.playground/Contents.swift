391119b2acf6a0e98fb73f7cf30c405a87fc023a
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-03-01 09:21:23 +0900

    day4

---
+++ DAY4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+//For loops
+//let count = 1...10
+//for number in count {
+//    print("Number is \(number)")
+//}
+//let albums = ["Red", "1989", "Reputation"]
+//for album in albums {
+//    print("\(album) is on Apple Music")
+//}
+//for _ in 1...5 {
+//    print("play")
+//}
+//let names = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+//for _ in names {
+//    print("[CENSORED] is a secred agent!")
+//}
+// While loops
+//var number = 1
+//while number <= 20 {
+//    print(number)
+//    number += 1
+//}
+//print("Ready of not, here I come!")
+var counter = 2
+while counter < 64 {
+    print("\(counter) is a power of 2.")
+    counter *= 2
+}
+var page: Int = 0
+while page <= 5 {
+    page += 1
+    print("I'm reading page \(page).")
+}
