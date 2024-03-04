fa7cefaa306657222d763754242fb3940564db38
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-03-05 08:56:40 +0900

    day4

--- DAY4/MyPlayground.playground/Contents.swift
+++ DAY4/MyPlayground.playground/Contents.swift
@@ -16,6 +16,7 @@ import UIKit
 //for _ in names {
 //    print("[CENSORED] is a secred agent!")
 //}
+
 // While loops
 //var number = 1
 //while number <= 20 {
@@ -23,13 +24,69 @@ import UIKit
 //    number += 1
 //}
 //print("Ready of not, here I come!")
-var counter = 2
-while counter < 64 {
-    print("\(counter) is a power of 2.")
-    counter *= 2
+//var counter = 2
+//while counter < 64 {
+//    print("\(counter) is a power of 2.")
+//    counter *= 2
+//}
+//var page: Int = 0
+//while page <= 5 {
+//    page += 1
+//    print("I'm reading page \(page).")
+//}
+
+// Repeat loops
+//var number = 1
+//repeat {
+//    print(number)
+//    number += 1
+//} while number <= 20
+//print("Ready or not, here I come!")
+            
+// Exiting loops
+var countDown = 10
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    
+    countDown -= 1
 }
-var page: Int = 0
-while page <= 5 {
-    page += 1
-    print("I'm reading page \(page).")
+print("Blast off!")
+
+// Exiting multiple loops
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        
+        print("\(i) * \(j) is \(product)")
+        
+        if (product == 50) {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+// Skipping items
+for i in 1...10 {
+    if i % 2 == 1 {
+        continue
+    }
+    
+    print(i)
+}
+
+// Infinite loops
+var counter = 0
+while true {
+    print(" ")
+    counter += 1
+    
+    if counter == 273 {
+        break
+    }
 }
