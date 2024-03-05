e04ca5fcc474e3f37d340f74ecdfbc4519289439
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-03-06 08:57:50 +0900

    day5

--- DAY5/MyPlayground.playground/Contents.swift
+++ DAY5/MyPlayground.playground/Contents.swift
@@ -41,3 +41,48 @@ func greet (_ person: String) {
     print("Hello, \(person)")
 }
 greet("Taylor")
+
+// Default parameters
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+greet("Taylor")
+greet("Taylor", nicely: false)
+
+// Variadic functions
+func square(numbers: Int...) {
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+square(numbers: 1, 2, 3, 4, 5)
+
+// Writing throwing functions
+enum PasswordError: Error {
+    case obvious
+}
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
+
+// Running throwing functions
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+// inout parameters
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+var myNum = 10
+doubleInPlace(number: &myNum)
