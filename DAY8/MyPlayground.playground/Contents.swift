ac34a7384c97bf56395ff575ccd6614dba274117
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-03-18 08:49:43 +0900

    day8

--- DAY8/MyPlayground.playground/Contents.swift
+++ DAY8/MyPlayground.playground/Contents.swift
@@ -12,6 +12,7 @@ print(tennis.name)
 */
 
 // 2. Computed properties
+/*
 struct Sport {
     var name: String
     var isOlympicSport: Bool
@@ -26,8 +27,9 @@ struct Sport {
 }
 let chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
 print(chessBoxing.olympicStatus)
-
+*/
 // 3. Property observers
+/*
 struct Progress {
     var task: String
     var amount: Int {
@@ -40,8 +42,9 @@ var progress = Progress(task: "Loading data", amount: 0)
 progress.amount = 30
 progress.amount = 80
 progress.amount = 100
-
+*/
 // 4. Methods
+/*
 struct City {
     var population: Int
     
@@ -51,3 +54,30 @@ struct City {
 }
 let london = City(population: 9_000_000)
 london.collectTaxes()
+*/
+// 5. Mutating methods
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+var person = Person(name: "Ed")
+person.makeAnonymous()
+print(person)
+
+// 6. Properties and methods of strings
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+// 7. Properties and methods of arrays
+var toys = ["Woody"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
